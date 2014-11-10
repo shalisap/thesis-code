8c1cc6d543ac71cf745ef7af8d881a2c0225534a
Shalisa Pattarawuttiwong <spattarawutt@gmail.com>
2014-11-10 10:06:51 -0500

    Extended SimpleKMeans. Prints out to an outfile

--- src/KMeans.java
+++ src/KMeans.java
@@ -3,78 +3,103 @@ import java.util.*;
 
 import weka.core.Instances;
 import weka.clusterers.SimpleKMeans;
+import weka.clusterers.ClusterEvaluation;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
 
-public class KMeans {
+/**
+ *  The KMeans class extends weka's SimpleKMeans and prints allows
+ *  for removal of an attribute as well as printing of additional parameter.
+ */
+public class KMeans extends SimpleKMeans{
 
-    public static void main(String[] args) throws Exception {
+    /**
+     * Removes the 'play' attribute from the weather.numeric.arff data set.
+     * @param  train     The data
+     * @return           The data without the 'play' attribute
+     * @throws Exception [description]
+     */
+    private static Instances remove_attribute(Instances train) throws Exception {
+            // remove play attribute
+            String[] remove_op = new String[2];
+            remove_op[0] = "-R"; // "range"
+            remove_op[1] = "5"; // fifth attribute ('play')
+            Remove remove = new Remove();
+            remove.setOptions(remove_op);
+            remove.setInputFormat(train);
+            return Filter.useFilter(train, remove);
+    }
 
-        // load data
-        try {
-            String infile = "./data/weather.numeric.arff";
-            BufferedReader reader = new BufferedReader(new FileReader(infile));
-            PrintWriter outfile = new PrintWriter(new FileWriter("./kMeans_output.txt"));
-            Instances data = new Instances(reader);
-
-            outfile.println("=== Run Information ===\n");
-            outfile.println("Data file: " + infile);
-            outfile.println("Instances: " + data.numInstances());
-
-            reader.close();
-
-            // create model
-            SimpleKMeans kmeans = new SimpleKMeans();
-            kmeans.setNumClusters(2);
-            kmeans.buildClusterer(data);
-
-            // print out clusters
-            // print general info
-            outfile.print(kmeans);
-
-            // print cluster centroids
-            outfile.println("===Cluster Centroids===\n");
-            Instances centroids = kmeans.getClusterCentroids();
-            for (int i = 0; i < centroids.numInstances(); i++) {
-                outfile.println("Centroid " + i + ": " + centroids.instance(i));
-            }
-
-            // print which cluster membership
-            outfile.println("\n===Cluster Members===");
-
-            HashMap<Integer, ArrayList<String>> clusters = new HashMap<Integer, ArrayList<String>>();
+    /**
+     * Outputs a hashmap of <Integer : ArrayList<String>> which represents <Cluster : ArrayList of Instances in the specific cluster>
+     * @param  newTrain  The data
+     * @return           The hashmap of <Integer : ArrayList<String>>
+     * @throws Exception [description]
+     */
+    private HashMap<Integer, ArrayList<String>> cluster_members(Instances newTrain) throws Exception {
+             HashMap<Integer, ArrayList<String>> clusters = new HashMap<Integer, ArrayList<String>>();
             // keep hashmap of <cluster, instance>
-            for (int i = 0; i < data.numInstances(); i++) {
-                int clusterNum = kmeans.clusterInstance(data.instance(i));
+            for (int i = 0; i < newTrain.numInstances(); i++) {
+                int clusterNum = clusterInstance(newTrain.instance(i));
 
                 if (clusters.containsKey(clusterNum)) {
                     ArrayList<String> group = clusters.get(clusterNum);
-                    group.add(data.instance(i).toString());
+                    group.add(newTrain.instance(i).toString());
                     clusters.put(clusterNum, group);
 
                 } else {
                     ArrayList<String> group = new ArrayList<String>();
-                    group.add(data.instance(i).toString());
+                    group.add(newTrain.instance(i).toString());
                     clusters.put(clusterNum, group);
                 }
             }
+            return clusters;
+    }
 
-            // print membership
-            for (Integer key: clusters.keySet()) {
-                ArrayList<String> group = clusters.get(key);
-                float percent = group.size() * 100f / data.numInstances();
-                outfile.println("\n==Cluster " + key + ": " + group.size() + " members (" + percent + "%)==\n");
-                for (String s: group) {
-                    outfile.println(s);
-                }
+    /**
+     * [kMeans description]
+     * @throws Exception [description]
+     */
+    public static void kMeans() throws Exception {
+
+        // load data
+        String infile = "./data/weather.numeric.arff";
+        BufferedReader reader = new BufferedReader(new FileReader(infile));
+        PrintWriter outfile = new PrintWriter(new FileWriter("./kMeans_output.txt"));
+        Instances train = new Instances(reader);
+        Instances newTrain = remove_attribute(train);
+        Instances test = newTrain;
+
+        outfile.println("=== Run Information ===\n");
+        outfile.println("Data file: " + infile);
+        outfile.println("Instances: " + newTrain.numInstances());
+
+        reader.close();
+
+        // create model
+        KMeans kmeans = new KMeans();
+        kmeans.setNumClusters(2);
+        kmeans.buildClusterer(newTrain);
+
+        // print out clusters
+        // print general info - evaluation
+        ClusterEvaluation eval = new ClusterEvaluation();
+        eval.setClusterer(kmeans);
+        eval.evaluateClusterer(test);
+        outfile.println(eval.clusterResultsToString());
+
+        // print which cluster membership
+        outfile.println("===Cluster Members===");
+        HashMap<Integer, ArrayList<String>> clusters = kmeans.cluster_members(newTrain);
+
+        for (Integer key: clusters.keySet()) {
+            ArrayList<String> group = clusters.get(key);
+            outfile.println("\n==Cluster " + key + ": " + group.size() + " members==\n");
+            for (String s: group) {
+                outfile.println(s);
             }
-
-            outfile.close();
-
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found. Exception thrown:" + e);
-        } catch (IOException e) {
-            System.out.println("Exception thrown:" + e);
         }
-
+        outfile.close();
     }
 
 }
--- src/TestingWeka.java
+++ src/TestingWeka.java
@@ -1,29 +1,9 @@
-import java.io.*;
-
-import weka.core.Instance;
-import weka.core.Instances;
-import weka.core.converters.ArffLoader;
-import weka.clusterers.Cobweb;
+//import KMeans;
 
 public class TestingWeka
 {
     public static void main(String[] args) throws Exception
     {
-        ArffLoader loader = new ArffLoader();
-        loader.setFile(new File("./data/weather.nominal.arff"));
-        Instances data = loader.getStructure();
-
-        Cobweb cw = new Cobweb(); // new instance of clusterer
-
-        //kmeans.setOptions(options); // set options
-        cw.buildClusterer(data); // build the clusterer
-
-        Instance current;
-        while ((current = loader.getNextInstance(data)) != null)
-            cw.updateClusterer(current);
-        cw.updateFinished();
-
-        System.out.println(cw);
-
+        KMeans.kMeans();
     }
 }
