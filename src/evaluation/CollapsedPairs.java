a442c998fe21a41d0f74214a767b05c7e31c2af0
Shalisa Pattarawuttiwong <spattarawutt@gmail.com>
2015-02-01 16:23:50 -0500

    Implementations for collapsed and distinguishing pairs

--- src/evaluation/CollapsedPairs.java
+++ src/evaluation/CollapsedPairs.java
@@ -1,19 +1,77 @@
 package evaluation;
 
+import java.util.ArrayList;
+
+import weka.core.Instances;
+
 /**
  * @author Shalisa Pattarawuttiwong
  *
  */
 public class CollapsedPairs extends AbstractEvaluation implements Evaluation {
 
+	protected Instances data;
+	protected int[] clusterAlg;
+	protected int[] groundTruth;
+	protected int numEvals;
+	
+	/**
+	 * Returns the total number of clusters for an assignment
+	 * @param clusters
+	 * @return number of clusters
+	 */
+	public int getNumClusters(int[] clusters) {
+		ArrayList<Integer> numClusterList = new ArrayList<Integer>();
+		int numClusters = 0;
+		for (int c: clusters) {
+			if (!numClusterList.contains(c)) {
+				numClusterList.add(c);
+				numClusters++;
+			}
+		}
+		return numClusters;
+	}
+	
 	/** 
+	 * a = the number of pairs that are distinguished in 
+	 * ground truth, and collapsed in the clustering algorithm
+	 * solution.
 	 * 
+	 * collapsed pairs = a / total number of pairs
+	 * 
+	 * An asymmetric distance.
 	 */
 	@Override
 	public double evaluate() {
-		// TODO Auto-generated method stub
-		return 0.0;
-
+		int numCollapsed = 0;
+    	for (int i = 0; i < groundTruth.length; i++) {
+    		for (int j = 0; j < groundTruth.length; j++) {
+    			if (groundTruth[i] != groundTruth[j] && clusterAlg[i] == clusterAlg[j]) {
+    				numCollapsed++;
+    			}
+    		}
+    	}
+		return numCollapsed / ((1/2) * (groundTruth.length - 1) * groundTruth.length);
 	}
 
+	/**
+	 * Constructor for CollapsedPairs
+	 */
+	public CollapsedPairs(int n, Instances d, int[] ca, int[] gt) {
+		this.data = d;
+		
+		// SHOULD THIS BE ALLOWED???
+		if (getNumClusters(ca) != getNumClusters(gt)) {
+    		throw new IllegalArgumentException("Clusters cannot have"
+    				+ "different number of total clusters");
+		}
+		this.clusterAlg = ca;
+		this.groundTruth = gt;
+		
+		if (n <= 0) {
+    		throw new IllegalArgumentException("The number of times evaluated"
+    				+ "cannot be fewer than 1.");
+		}
+		this.numEvals = n;
+	}
 }
--- src/evaluation/DistinguishingPairs.java
+++ src/evaluation/DistinguishingPairs.java
@@ -16,6 +16,17 @@ public class DistinguishingPairs extends AbstractEvaluation implements
 	protected int[] cluster2;
 	protected int numEvals;
 	
+	private int same;
+	private int diff;
+	private int same1diff2;
+	private int diff1same2;
+	
+	private boolean adjRandIndex = false;
+	
+	private void setAdjRandIndex(boolean a) {
+		adjRandIndex = a;
+	}
+	
 	/**
 	 * Returns the total number of clusters for an assignment
 	 * @param clusters
@@ -33,43 +44,6 @@ public class DistinguishingPairs extends AbstractEvaluation implements
 		return numClusters;
 	}
 	
-	 /**
-     * Determines the actual values in each cluster. 
-     * @param clusters
-     * @param 
-     */
-    public ArrayList<ArrayList<String>> determineClusters(int[] clusters) {
-    	ArrayList<ArrayList<String>> actualClusters = new ArrayList<ArrayList<String>>();
-    	HashMap<Integer, ArrayList<String>> clusterValues = new HashMap<Integer, ArrayList<String>>();
-    	int numInst = 0;
-    	for (int i : clusters) {
-    		if (!clusterValues.containsKey(i)) {
-    			ArrayList<String> c = new ArrayList<String>();
-    			c.add(data.instance(numInst).toString());
-    			clusterValues.put(i, c);
-    		} else {
-    			ArrayList<String> c = clusterValues.get(i);
-    			c.add(data.instance(numInst).toString());
-    			clusterValues.put(i, c);
-    		}
-    		numInst++;
-    	}
-    	
-    	for (int k: clusterValues.keySet()) {
-			ArrayList<String> c = clusterValues.get(k);
-    		Collections.sort(c);
-    		actualClusters.add(c);
-    	}
-    	
-    	Collections.sort(actualClusters, new Comparator<ArrayList<String>>() {
-    		public int compare(ArrayList<String> a, ArrayList<String> b) {
-    			return a.get(0).compareTo(b.get(0));
-    		}
-    	});
-    	
-    	return actualClusters;
-    }
-	
 	/** 
 	 * The Rand Index, As described in Rand (1971), given N points, X1, X2, ... , XN, and
 	 * two clusterings of them Y = {Y1 ... YK1} and Y' = {Y1' ... YK1'}, 
@@ -80,45 +54,94 @@ public class DistinguishingPairs extends AbstractEvaluation implements
 	 * 
 	 * Simplified: 
 	 * a = number of pairs of elements in N that are in the same set in X and Y
-	 * b = number of pairs of elements in N that are in different sets in X and 
-	 * 		different sets in Y.
-	 * c = number of pairs of elements in N that are in the same set in X and 
+	 * b = number of pairs of elements in N that are in the same set in X and 
 	 * 		different sets in Y
-	 * d = number of pairs of elements in N that are in different sets in X and in the 
+	 * c = number of pairs of elements in N that are in different sets in X and in the 
 	 * 		same set in Y.
+	 * d = number of pairs of elements in N that are in different sets in X and 
+	 * 		different sets in Y.
 	 * 
-	 * c(Y, Y') = a + b / a + b + c + d = a + b / (N choose 2)
+	 * c(Y, Y') = a + d / a + b + c + d = a + d / (N choose 2)
 	 * 
 	 * c = 0 when two clusterings have no similarities -> 1 where they're identical.
+	 * 
+	 * the Adjusted Rand Index = (n choose 2)(a + d) - [(a+b)(a+c) + (c+d)(b+d)] /
+	 * 							(n choose 2)^2 - [(a+b)(a+c) + (c+d)(b+d)]
 	 */
 	@Override
 	public double evaluate() {
 		// determine actual sorted clusters
-		ArrayList<ArrayList<String>> actualCluster1 = determineClusters(cluster1);
-		ArrayList<ArrayList<String>> actualCluster2 = determineClusters(cluster2);
-    	int same = 0;
-    	int diff = 0;
-    	for (int i = 0; i < actualCluster1.size(); i++) {
-    		for (int j = 0; j < actualCluster1.get(i).size(); j++) {
-    			// cannot directly compare...
-    			if (actualCluster1.get(i).get(j) == actualCluster2.get(i).get(j)) {
-    				
+    	same = 0;
+    	diff = 0;
+    	same1diff2 = 0;
+    	diff1same2 = 0;
+    	for (int i = 0; i < cluster1.length; i++) {
+    		for (int j = 0; j < cluster1.length; j++) {
+    			// if i and j in cluster 1 are in the same set and i and j in cluster 2 are in the same set 
+    			if (cluster1[i] == cluster1[j] && cluster2[i] == cluster2[j]) {
+    				same++;
+    			} else if (cluster1[i] != cluster1[j] && cluster2[i] != cluster2[j]){
+    				diff++;
+    			} else if(cluster1[i] == cluster1[j] && cluster2[i] != cluster2[j]) {
+    				same1diff2++;
+    			} else if (cluster1[i] != cluster1[j] && cluster2[i] == cluster2[j]) {
+    				diff1same2++;
     			}
-    		}
+    		}		
     	}
-		return 0.0;
-
+    	if (adjRandIndex == true) {
+    		return getAdjustedRandIndex();
+    	} else {
+    		return getRandIndex();
+    	}
 	}
 	
-	public DistinguishingPairs(int n,Instances d, int[] a, int[] b) {
+	/**
+	 * The Rand Index, a + d / (N choose 2), where 
+	 * a = number of pairs of elements that are in the same set in X and Y
+	 * d = number of pairs of elements that are in different sets in X and 
+	 * 		different sets in Y.
+	 * 
+	 * @return Rand Index
+	 */
+	private double getRandIndex() {
+		return (same + diff) / ((1/2) * (cluster1.length - 1) * cluster1.length);
+	}
+	
+	/**
+	 * The Adjusted Rand Index, (n choose 2)(a + d) - [(a+b)(a+c) + (c+d)(b+d)] /
+	 * 							(n choose 2)^2 - [(a+b)(a+c) + (c+d)(b+d)], where
+	 * a = number of pairs of elements that are in the same set in X and Y
+	 * b = number of pairs of elements that are in the same set in X and 
+	 * 		different sets in Y
+	 * c = number of pairs of elements that are in different sets in X and in the 
+	 * 		same set in Y.
+	 * d = number of pairs of elements that are in different sets in X and 
+	 * 		different sets in Y.
+	 *
+	 * @return Adjusted Rand Index
+	 */
+	private double getAdjustedRandIndex() {
+		double nchoose2 = (1/2) * (cluster1.length - 1) * cluster1.length;
+		double sum = ((same + same1diff2) * (same + diff1same2) +
+				(diff1same2 + diff) * (same1diff2 + diff));
+		return (nchoose2 * (same + diff) - sum) / 
+				(Math.pow(nchoose2, 2) - sum);
+	}
+	
+	/**
+	 * Constructor for DistinguishingPairs
+	 */
+	public DistinguishingPairs(int n, Instances d, int[] a, int[] b) {
 		this.data = d;
 		
-		if (getNumClusters(cluster1) != getNumClusters(cluster2)) {
+		if (getNumClusters(a) != getNumClusters(b)) {
     		throw new IllegalArgumentException("Clusters cannot have"
     				+ "different number of total clusters");
+		}
 		this.cluster1 = a;
 		this.cluster2 = b;
-
+		
 		if (n <= 0) {
     		throw new IllegalArgumentException("The number of times evaluated"
     				+ "cannot be fewer than 1.");
