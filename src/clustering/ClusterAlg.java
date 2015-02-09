9999fe0f9764fcedc0aacdc6f66c8e5478f20f4d
Shalisa Pattarawuttiwong <spattarawutt@gmail.com>
2015-02-09 18:24:53 -0500

    Edited javadoc methods for ClusterAlg. Fixes #1

--- src/clustering/ClusterAlg.java
+++ src/clustering/ClusterAlg.java
@@ -8,19 +8,17 @@ package clustering;
 public interface ClusterAlg {
 
 	/**
-	 * Given instances, [x0, x1, ..., xn], and number of clusters, y > 0,
-	 * assigns a cluster number 0...y to the instance, such that
-	 * each instance maps to a cluster 
-	 * (e.g. [(0...y)0, (0...y)1,...,(0...y)n] of size n).
+	 * Given instances (data), D = [x{0}, x{1}, ..., x{n-1}], a 
+	 * similarity/distance measure, and number of clusters
+	 * to generate, k > 0, partitions the data such that there are 
+	 * C{0}, ..., C{k-1} clusters of D.
 	 */
     public void cluster();
 
     /**
-     * 
-     * @return assignment of instances to clusters, 
-     * 		[(0...y)0, (0...y)1,..., (0...y)n] where 
-     * 		it is mapped to given instances [x0, x1, ..., xn] and
-     * 		each 0...y is the cluster number the instance is assigned to and . 
+     * Returns clusters [i{0}, ..., i{n-1}] where i{j} is a member of C{i},
+     * where C{0}, ..., C{k-1} are partitions of D.
+     * @return assignment of instances of the dataset to clusters.
      */
     public int[] getClusters();
 
